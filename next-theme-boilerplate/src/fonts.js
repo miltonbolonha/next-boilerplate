88cdcecf5d7b5d57565c17c9b934ca14ecc5fa82
miltonbolonha <miltonbolonha@gmail.com>
2023-07-07 18:46:28 -0300

    bugfix/gitignore next workspace

---
+++ next-theme-boilerplate/src/fonts.js
@@ -0,0 +1,14 @@
+import {
+  Roboto_Mono,
+  Redressed,
+  Jetbrains_Mono,
+  Josefin_Sans,
+  Quattrocento,
+  Playfair_Display,
+  Nova_Flat,
+} from "next/font/google";
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
