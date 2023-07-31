b723f643b2d4f5496e2f6f9826cae4c0898aa2db
miltonbolonha <miltonbolonha@gmail.com>
2023-07-31 00:56:45 -0300

    feature/release

--- next-theme-boilerplate/src/components/loader.js
+++ next-theme-boilerplate/src/components/loader.js
@@ -1,6 +1,7 @@
 "use client";
 
 export default function myImageLoader({ src }) {
+  console.log(process.env);
   const githubRepo = process.env.GITHUB_REPOSITORY || false;
   const loaderPrefix = githubRepo ? githubRepo.split("/")[1] : false;
   return `${loaderPrefix || ""}${src}`;
