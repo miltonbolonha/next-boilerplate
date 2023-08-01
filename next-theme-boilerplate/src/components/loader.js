44185a6a1ae4eec4b1efee832a3ee2b4a05755c5
miltonbolonha <miltonbolonha@gmail.com>
2023-08-01 08:46:32 -0300

    feature/env created ghpages action workflow

--- next-theme-boilerplate/src/components/loader.js
+++ next-theme-boilerplate/src/components/loader.js
@@ -1,8 +1,11 @@
 "use client";
 
 export default function myImageLoader({ src, width, height, quality }) {
-  const loaderPrefix = process.env.LOADER_PREFIX || false;
-  return `${loaderPrefix || ""}${src}?w=${width}&h=${height}&q=${
-    quality || 100
-  }`;
+  const objReturn = `${src}?w=${width}&h=${height}&q=${quality || 100}`;
+  const githubRepo = process.env.GITHUB_REPO || false;
+  if (!githubRepo) {
+    return objReturn;
+  }
+
+  return githubRepo + "/" + objReturn;
 }
