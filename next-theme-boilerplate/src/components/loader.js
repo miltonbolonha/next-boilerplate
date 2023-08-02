f88524387c040fce72caccbe51acad3471dfd196
miltonbolonha <miltonbolonha@gmail.com>
2023-08-02 03:02:27 -0300

    bugfix/loader prefix

--- next-theme-boilerplate/src/components/loader.js
+++ next-theme-boilerplate/src/components/loader.js
@@ -3,8 +3,8 @@
 export default function myImageLoader({ src, width, height, quality }) {
   console.log("GITHUB_PAGES");
   console.log(process.env.GITHUB_PAGES);
-  console.log("GITHUB_REPO");
-  console.log(process.env.GITHUB_REPOSITORY);
+  console.log("NEXT_GITHUB_REPO");
+  console.log(process.env.NEXT_GITHUB_REPO);
   const objReturn = `${src}?w=${width}&h=${height}&q=${quality || 100}`;
   const githubRepo = process.env.GITHUB_REPOSITORY || false;
   if (!githubRepo) {
