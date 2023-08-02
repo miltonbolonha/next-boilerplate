de2a9356baf2f8b7eb0916ba4af55fd8e4e2d2f1
miltonbolonha <miltonbolonha@gmail.com>
2023-08-02 13:37:30 -0300

    bugfix/dotEnv workflow

--- next-theme-boilerplate/src/components/loader.js
+++ next-theme-boilerplate/src/components/loader.js
@@ -5,6 +5,8 @@ export default function myImageLoader({ src, width, height, quality }) {
   console.log(process.env.GITHUB_PAGES);
   console.log("NEXT_GITHUB_REPO");
   console.log(process.env.NEXT_GITHUB_REPO);
+  console.log("process.env.NEXT_GITHUBs_REPO 2222");
+  console.log(process.env.NEXT_GITHUBs_REPO);
   const objReturn = `${src}?w=${width}&h=${height}&q=${quality || 100}`;
   const githubRepo = process.env.GITHUB_REPOSITORY || false;
   if (!githubRepo) {
