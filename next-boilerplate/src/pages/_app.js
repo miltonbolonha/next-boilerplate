4dda1ec81f36a72df2ad225effb804d482f82be7
miltonbolonha <miltonbolonha@gmail.com>
2023-08-07 15:15:10 -0300

    remove recaptcha

--- next-boilerplate/src/pages/_app.js
+++ next-boilerplate/src/pages/_app.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 import Head from "next/head";
 import "@fontsource/varela";
@@ -19,33 +18,29 @@ const inter = Inter({ subsets: ["latin"] });
 
 function App({ Component, pageProps }) {
   return (
-    <ReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-    >
-      <ThemeProvider>
-        <Head>
-          <meta name='theme-color' content='#06092B' />
-          <meta
-            name='description'
-            content='Um blog de um desenvolvedor FullStack (LINUX, PHP & NODEJS), fã de rock, política, gatos e inteligência artificial.'
-          />
-        </Head>
-        <DefaultSeo {...SEO} />
+    <ThemeProvider>
+      <Head>
+        <meta name='theme-color' content='#06092B' />
+        <meta
+          name='description'
+          content='Um blog de um desenvolvedor FullStack (LINUX, PHP & NODEJS), fã de rock, política, gatos e inteligência artificial.'
+        />
+      </Head>
+      <DefaultSeo {...SEO} />
 
-        <div className={inter.className}>
-          <Layout>
-            <NextNProgress
-              color='#F231A5'
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-              showSpinner={false}
-            />
-            <Component {...pageProps} />
-          </Layout>
-        </div>
-      </ThemeProvider>
-    </ReCaptchaProvider>
+      <div className={inter.className}>
+        <Layout>
+          <NextNProgress
+            color='#F231A5'
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={5}
+            showSpinner={false}
+          />
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </ThemeProvider>
   );
 }
 
