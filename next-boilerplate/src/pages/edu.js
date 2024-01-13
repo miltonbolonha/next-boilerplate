02635e01df88ee5337f7a80506980263de275558
Milton Bolonha <miltonbolonha@gmail.com>
2024-01-13 14:36:40 -0300

    Update edu.js

--- next-boilerplate/src/pages/edu.js
+++ next-boilerplate/src/pages/edu.js
@@ -35,7 +35,7 @@ const Edu = () => (
       <Row opt={{ isBoxed: true }}>
         <div className='hero-txt'>
           <h1>Matrial Didático</h1>
-          <h2>Página em Construção, entre em contato direto com o autor: 16 991997935</h2>
+          <h2>Página em Construção, entre em contato direto com o autor: 12 98106-2959</h2>
         </div>
         <ul className='contact-link-tree-wrapper'>
           <li>Carta de boas-vindas</li>
